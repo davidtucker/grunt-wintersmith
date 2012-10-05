ac51e8253c4695fb0ff44409d83dcbbab7a4f809
adam j. sontag <ajpiano@ajpiano.com>
2012-10-05 02:53:55 -0400

    temporary path resolution hack

--- tasks/wintersmith.js
+++ tasks/wintersmith.js
@@ -1,11 +1,19 @@
 module.exports = function(grunt) {
 
-  var wintersmith = require("wintersmith");
+  var wintersmith = require("wintersmith"),
+  pluginsLoaded = false;
 
   grunt.registerTask("wintersmith", "Use the wintersmith static site generator", function() {
     var config = grunt.config("wintersmith"),
-    done = this.async();
-    
+    done = this.async(),
+    cwd = process.cwd();
+    // This "pluginsLoaded" and concatenation of cwd is a dirty hack to get around some relative path resolution that I can't quite resolve yet
+    if ( config.plugins && !pluginsLoaded ) {
+      config.plugins.forEach(function(path, i) {
+        config.plugins[i] = cwd + path;
+      });
+      pluginsLoaded = true;
+    }
     grunt.helper("wintersmith", config, function(error) {
       if ( error ) {
         grunt.warn("Wintersmithing failed: "+error);
